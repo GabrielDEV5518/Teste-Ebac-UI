c7aaff3148e3435809e4856a8b047a62814c15c6
Gabriel <gdbp20@gmail.com>
2024-10-22 18:24:47 -0300

    Arquivo de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,30 +1,31 @@
 /// < reference types="cypress"/>
 
+const perfil = require('../../fixtures/perfil.json')
+
 describe('funcionalidade: login', () => {
 
     beforeEach(() => {
-     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+     cy.visit('my-account')
 });
 
-    afterEach(() => {
-     cy.screenshot() 
-});
+    afterEach(() => {     cy.screenshot() 
+  });
 
 
 
     it( 'Deve fazer o login com sucesso', () => {
-         cy.get('#username').type('gabriel-teste@gmail.com')
-         cy.get('#password').type('32247443gb')
+         cy.get('#username').type(perfil.usuario)
+         cy.get('#password').type(perfil.senha)
          cy.get('.woocommerce-form > .button').click()
-         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'gabriel-teste (não é gabriel-teste? Sair)')
+         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
     })
 
 
-    it.only('deve exibir uma mensagem de erro ao inserir um usúario invalido', () => {
+    it('deve exibir uma mensagem de erro ao inserir um usúario invalido', () => {
          cy.get('#username').type('chuchu@gmail.com')
          cy.get('#password').type('32247443gb')
          cy.get('.woocommerce-form > .button').click()
-         cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido.')
+         cy.get('.woocommerce-error').should('exist')
     });
 
 
@@ -32,10 +33,21 @@ describe('funcionalidade: login', () => {
          cy.get('#username').type('gabriel-teste@gmail.com')
          cy.get('#password').type('000000')
          cy.get('.woocommerce-form > .button').click()
-         cy.get('.woocommerce-error').should('contain' , 'A senha fornecida para o e-mail gabriel-teste@gmail.com está incorreta.')
+         cy.get('.woocommerce-error').should('exist')
     });     
 
     
+    it( 'Deve fazer o login com sucesso - Usando Fixture', () => {
+         cy.fixture('perfil').then( dados => {
+         cy.get('#username').type(perfil.usuario, {log:false})
+         cy.get('#password').type(perfil.senha , {log: false})
+         cy.get('.woocommerce-form > .button').click()
+         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+
+    })
+
+
+    })
 
 })
-
